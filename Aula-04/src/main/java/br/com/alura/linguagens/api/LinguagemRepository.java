06f12685e8ebc305becff024ec7d888bcdaf27b6
Douglas dos Santos da Silva <129339869+eudougs@users.noreply.github.com>
2023-04-02 14:56:37 -0300

    Add files via upload

---
+++ Aula-04/src/main/java/br/com/alura/linguagens/api/LinguagemRepository.java
@@ -0,0 +1,8 @@
+package br.com.alura.linguagens.api;
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
+    List<Linguagem> findByOrderByRanking();
+}
